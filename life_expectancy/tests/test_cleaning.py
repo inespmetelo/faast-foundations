5cd55682222fdebed5ef0c6b307bfe1b47b50138
inespmetelo <ines.metelo@nos.pt>
2026-01-06 12:24:10 +0000

    refactor the code

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,7 +1,7 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import main as clean_data
 from . import OUTPUT_DIR
 
 
@@ -9,7 +9,8 @@ def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
     clean_data(country="PT")
     pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv"
+        OUTPUT_DIR / "pt_life_expectancy.csv",
+        
     )
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
