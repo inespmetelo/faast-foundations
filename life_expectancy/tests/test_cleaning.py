5d27fffb9fa215a43d5a7d207968eb17ef8f65d1
inespmetelo <ines.metelo@nos.pt>
2026-01-26 18:16:50 +0000

    linter

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -9,8 +9,7 @@ def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
     clean_data(country="PT")
     pt_life_expectancy_actual = pd.read_csv(
-        OUTPUT_DIR / "pt_life_expectancy.csv",
-        
+        OUTPUT_DIR / "pt_life_expectancy.csv"
     )
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
